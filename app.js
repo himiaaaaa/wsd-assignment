781e365cd39411dd865838e28619f2273741a52b
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2023-12-14 10:50:26 +0200

    3.2 the basics of debugging Deno applications

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const handleRequest = (request) => {
-    console.log(`Request method: ${request.method}`);
-    console.log(`Request url: ${request.url}`);
-    return new Response("Hello world!");
+  return new Response("Hello world!");
 };
-  
+
 Deno.serve(handleRequest);
+
+//deno fmt app.js
+/* 
+let x = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+console.log(x, b, c, d);
+*/
+
+//deno lint app.js
+/* const a = 1;
+
+if (a === 1) {
+  console.log("Hello!");
+} */
