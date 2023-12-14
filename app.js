984ba83808b6795cfa768267002cfbad69f31a01
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2023-12-14 20:46:35 +0200

    4.4 Request Parameters

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 import { Hono } from "https://deno.land/x/hono@v3.7.4/mod.ts";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => c.text("The starting point."))
-app.post('/', (c) => c.text("Postman pat."))
-app.get('/it', (c) => c.text("I think so."))
-app.on('cat', '/secrets', (c) => c.text("Meow!"))
-app.on('whats', '/up', (c) => c.text("A movie!"))
+app.get("/", (c) => {
+  let operation = c.req.query("operation")
+  let num1 = Number(c.req.query("number1"))
+  let num2 = Number(c.req.query("number2"))
 
-export default app
+  if(operation === "sum" && num1 && num2){
+    return c.text(`${num1 + num2}`)
+  } else if (operation === "difference" && num1 && num2) {
+    return c.text(`${num1 - num2}`)
+  } else {
+    return c.text("Invalid parameters.")
+  }
+});
+
+export default app;
 
 
 
