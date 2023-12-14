32aac36975ebe9b49ae27a421d3df6279a3eed2c
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2023-12-14 18:24:41 +0200

    Update 3.5.Paths_and_Methods.js

---
+++ assignment archive/3.First_Web_Applications/3.5.Paths_and_Methods.js
@@ -0,0 +1,20 @@
+//3.5.1 exercise
+/* const handleRequest = (request) => {
+  const url = new URL(request.url);
+  return new Response(`${request.method} request made to path ${url.pathname}`);
+};
+Deno.serve(handleRequest);
+*/
+
+//3.5.2 exercise
+const handleRequest = (request) => {
+    const url = new URL(request.url)
+  
+    if (url.pathname === "/secret" && request.method === "PEEK"){
+      return new Response("Peeking at secret data...")
+    } else {
+      return new Response("There is nothing to see here...")
+    }
+  }
+  
+  Deno.serve(handleRequest)
