6ea03103f8b38c25ba916b81340c8b0bd267d82b
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2023-12-14 11:50:52 +0200

    handle requests to paths

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
+/* 
+//3.3.1 exercises - responding with the path
 const handleRequest = (request) => {
-  return new Response("Hello world!");
+    const url = new URL(request.url)
+
+    return new Response(`You requested the path ${url.pathname}`);
+};
+
+Deno.serve(handleRequest); */
+
+
+////3.3.2 exercises - yksi, kaksi, pong
+const handleRequest = (request) => {
+    const url = new URL(request.url)
+
+    let message = null
+    if(url.pathname === "/one"){
+        message = "yksi"
+    } else if(url.pathname === "/two"){
+        message = "kaksi"
+    } else {
+        message = "pong"
+    }
+
+    return new Response(message);
 };
 
 Deno.serve(handleRequest);
 
-//deno fmt app.js
-/* 
-let x = 1;
-let b = 2;
-let c = 3;
-let d = 4;
-console.log(x, b, c, d);
-*/
-
-//deno lint app.js
-/* const a = 1;
-
-if (a === 1) {
-  console.log("Hello!");
-} */
