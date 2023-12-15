f9eb9ccf3b9e10c2b10119f4e48b0465e06036c0
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2023-12-15 19:51:57 +0200

    6.2.1 key value stores

--- assignment archive/6.Data_on_Server/6.2.1.key_value_stores/app.js
+++ assignment archive/6.Data_on_Server/6.2.1.key_value_stores/app.js
@@ -4,14 +4,12 @@ import * as store from "./store.js";
 const app = new Hono();
 
 app.get("/", async(c) => {
-  if (c.req.query("store")) {
 
+  if (c.req.query("store")) {
     await store.setStore(c.req.query("store"))
     const store_value = await store.getStore()
     return c.text(`Store: ${store_value}`)
-
   }
-  
   const store_value = await store.getStore()
   return c.text(`Store: ${store_value}`)
 })
